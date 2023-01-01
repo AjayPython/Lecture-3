c756035a11dac2604caeb57f2f5cbbca5d66390d
AjayPython <ajay.shukla20@gmail.com>
2023-01-01 20:39:34 +0530

    Second Commit

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "4.48.0"
     }
   }
@@ -10,4 +10,21 @@ terraform {
 provider "aws" {
   # Configuration options
   region = "us-east-1"
+}
+
+data "aws_subnet" "selected" {
+  filter {
+    name   = "tag:Name"
+    values = ["Subnet_1"]
+  }
+}
+
+resource "aws_instance" "ec2_1" {
+  ami           = "ami-0b5eea76982371e91"
+  instance_type = "t2.micro"
+  subnet_id     = data.aws_subnet.selected.id
+  key_name      = "jyotisna1979"
+  tags = {
+    Name = "HelloWorld"
+  }
 }
